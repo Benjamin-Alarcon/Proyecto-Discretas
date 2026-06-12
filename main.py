8e29bab319ecfc38f4495267d16367f49580ae33
Benjamin-Alarcon <balarcon2026@alu.uct.cl>
2026-06-11 22:34:20 -0400

    ventana

---
+++ main.py
@@ -0,0 +1,15 @@
+import customtkinter as ctk
+"""
+El sabado vamos a hacer el diagrama de flujo y un
+bosquejo del diseño, veremos que algoritmos usar y asi
+"""
+
+#Creacion Ventana
+root = ctk.CTk()
+
+#Ajustes de tamaño en Px y titulo del programa
+root.geometry("800x800")
+root.title("Proyecto Matematicas Discretas")
+
+#Bucle para mantener la ventana abierta
+root.mainloop()
